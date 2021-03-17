7d53ce299884e4bb24c1708fe4fabb84e2031df7
Corentin <33163342+corentinmusard@users.noreply.github.com>
2021-03-17 18:00:43 +0000

    Update FamiliaController.php

--- src/Controller/FamiliaController.php
+++ src/Controller/FamiliaController.php
@@ -15,7 +15,7 @@ class FamiliaController extends AbstractController
     public function index(Request $request): Response
     {
         return $this->render('familia.html.twig', [
-            'mercredi' => (new \DateTime("now") > new \DateTime("2021-03-17 18:00:00")),
+            'mercredi' => (new \DateTime("now") > new \DateTime("2021-03-10 18:00:00")),
             'jeudi' => (new \DateTime("now") > new \DateTime("2021-03-18 18:00:00")),
         ]);
     }
