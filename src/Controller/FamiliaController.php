983a1342e5bee7facd10f81d6fe2e746954dcbb1
Clem-Duchesne <61272427+Clem-Duchesne@users.noreply.github.com>
2021-03-18 18:05:07 +0100

    Update FamiliaController.php

--- src/Controller/FamiliaController.php
+++ src/Controller/FamiliaController.php
@@ -16,7 +16,7 @@ class FamiliaController extends AbstractController
     {
         return $this->render('familia.html.twig', [
             'mercredi' => (new \DateTime("now") > new \DateTime("2021-03-10 18:00:00")),
-            'jeudi' => (new \DateTime("now") > new \DateTime("2021-03-18 18:00:00")),
+            'jeudi' => (new \DateTime("now") > new \DateTime("2021-03-10 18:00:00")),
         ]);
     }
 }
