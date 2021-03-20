75560135b9a901608148f0bc32e1cbc862c1370b
Corentin Musard <corentinmusard@gmail.com>
2021-03-20 16:36:41 +0100

    code

--- src/Controller/DecodeurController.php
+++ src/Controller/DecodeurController.php
@@ -30,7 +30,25 @@ class DecodeurController extends AbstractController
             $data = $form->getData();
 
             $decode = [
-                "foo" => "bar",
+                "196" => "La Famille Moneta te met au défi de te prendre en photo devant 3 de nos Partenaires (adresses présentes sur le site). Afin de valider ton défi, poste cette photo sur notre groupe Mafiatech 2021 - Groupe privé avec le #défiMoneta."
+
+            ,"167" => "La Famille Fiestichita te met au défi de réaliser une recette de cocktail et de la poster sous forme de vidéo sur notre groupe Mafiatech 2021 - Groupe privé avec le #défiFiestichita."
+
+            ,"197" => "La Famille PreventaDDoRS te met au défi de faire boire de l’eau à un polypote entre 2h et 6h du matin. Afin de valider ton défi, poste une preuve vidéo sur notre groupe Mafiatech 2021 - Groupe privé avec le #défiPreventaDDoRS."
+
+
+            ,"784" => "La Famille Socioleta te met au défi de retrouver les symboles qu’elle a dissimulé un peu partout dans sa communication. Pour réussir ce défi tu devras te laisser guider par les indices que nous t’avons laissés. Chaque fois que tu trouveras un symbole, reviens ici pour rentrer son code correspondant.
+            Premier indice : 5pts, 10pts, 20pts, 50pts, [100pts]"
+
+            ,"294" => "Indice : Un caisson c’est lourd ; avec des musiques adaptées bien-sûr."
+
+            ,"734" => "Indice : Le nôtre n’a pas leak. (À ta place je m’amuserais à cliquer partout)"
+
+            ,"134" => "Quatrième indice : Réseau social chinois. (Si tu ne l’as pas tu peux juste aller cliquer sur le lien dans le taplink)"
+
+            ,"524" => "Bravo tu arrives à la fin de ce jeu de piste ! Maintenant rentre les numéros des symboles, dans le meme ordre que tu les as trouvés."
+
+            ,"294734134524" => "Afin de valider ton défi, il faut que tu remplisses ce formulaire : https://docs.google.com/forms/d/e/1FAIpQLScR8xmuJDa6IxOLFmIWDH0K4hw6CHFboq8IjANi0HkZhZ-sQA/viewform?usp=sf_link"
             ];
 
             if (array_key_exists($data['Decodeur'], $decode)) {
